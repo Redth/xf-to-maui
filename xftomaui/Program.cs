da88dddf9df4b7d5815e48a8a302e620698eef08
Jonathan Dick <jond@hey.com>
2021-02-23 08:50:21 -0500

    Fix path replacement for nested subdirs

--- xftomaui/Program.cs
+++ xftomaui/Program.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Text.RegularExpressions;
 using System.Linq;
+using Cake.Common;
 using Cake.Common.IO;
 using Cake.Core;
 using Cake.Core.Diagnostics;
@@ -13,7 +14,8 @@ using OfficeOpenXml;
 
 public static class Consts
 {
-    public const string BasePath = "C:/xamarin/Xamarin.Forms/"; // "/Users/redth/code/Xamarin.Forms/";
+    //public const string BasePath = "C:/xamarin/Xamarin.Forms/"; // "/Users/redth/code/Xamarin.Forms/";
+    public const string BasePath = "/Users/redth/code/Xamarin.Forms/";
     public const string NameMappingsXlsx = "./NameMappings.xlsx";
 }
 
@@ -45,12 +47,16 @@ public sealed class DefaultTask : FrostingTask<BuildContext>
 {
     public override void Run(BuildContext context)
     {
+        // Clean up repo to start fresh
+        context.StartProcess("git", new ProcessSettings{ Arguments = $"-C \"{Consts.BasePath}\" reset --hard" });
+        context.StartProcess("git", new ProcessSettings{ Arguments = $"-C \"{Consts.BasePath}\" clean -xdf" });
+
         ReplaceMappings(context, Consts.NameMappingsXlsx, new [] {
-            ("Maui.Essentials", $"{Consts.BasePath}src/Essentials/**/*.cs" ),
-            ("Maui.Compatibility", $"{Consts.BasePath}src/Platform.Renderers/**/*.cs" ),
-            ("Maui.Controls", $"{Consts.BasePath}src/Forms/**/*.cs" ),
-            ("Maui.Controls", $"{Consts.BasePath}src/Controls/**/*.cs" ),
-            ("Maui.Core", $"{Consts.BasePath}src/Platform.Handlers/**/*.cs" ),
+            ("Maui.Essentials", Consts.BasePath + "src/Essentials/**/*.{cs,xaml}" ),
+            ("Maui.Compatibility", Consts.BasePath + "src/Platform.Renderers/**/*.{cs,xaml}" ),
+            ("Maui.Controls", Consts.BasePath + "src/Forms/**/*.{cs,xaml}" ),
+            ("Maui.Controls", Consts.BasePath + "src/Controls/**/*.{cs,xaml}" ),
+            ("Maui.Core", Consts.BasePath + "src/Platform.Handlers/**/*.{cs,xaml}" ),
         });
 
 
@@ -69,8 +75,34 @@ public sealed class DefaultTask : FrostingTask<BuildContext>
         FixSlns(context, filenameMapping, directoryMapping);
         
         DeleteEmptyDirectories(context, Consts.BasePath);
+
+
+        if (context.FileExists("./.github/CODEOWNERS"))
+            context.DeleteFile("./.github/CODEOWNERS");
     }
 
+    static IEnumerable<(string from, string to)> GetNamespaceMappings(ICakeContext context, FilePath xlsxFile, params string[] sheets)
+    {
+        var namespaceMapping = new Dictionary<string, string>();
+
+        foreach (var sheet in sheets)
+        {
+            var setData = ReadExcel(context, xlsxFile, sheet, true);
+
+            foreach (var row in setData)
+            {
+                var fromNs = row?[0];
+                var toNs = row?[1];
+
+                if (!string.IsNullOrEmpty(fromNs) && !string.IsNullOrEmpty(toNs))
+                    namespaceMapping.TryAdd(fromNs, toNs);
+            }
+        }
+
+        var ordered = namespaceMapping.OrderByDescending(kvp => kvp.Key?.Split('.', 1000)?.Length ?? 0);
+
+        return ordered.Select(o => (o.Key, o.Value));
+    }
 
     static void FixRelativeFileReferences(ICakeContext context, IEnumerable<(string from, string to)> directoryMappings, IEnumerable<(string from, string to)> fileMappings)
     {
@@ -98,6 +130,9 @@ public sealed class DefaultTask : FrostingTask<BuildContext>
                 {
                     var refRelativePath = new FilePath(rp);
 
+                    if (file.FullPath.Contains("Maps.macOS"))
+                        context.Log.Information("MapsMacos");
+                        
                     if (file.Segments.Contains("eng"))
                         continue;
                     if (!context.FileExists(refRelativePath.MakeAbsolute(file.GetDirectory())))
@@ -109,10 +144,38 @@ public sealed class DefaultTask : FrostingTask<BuildContext>
                     // Get the relative directory path for the current value
                     var refDirRelativePath = refFullPath.GetDirectory().FullPath.Replace(Consts.BasePath, "./").TrimEnd('/', '\\');
 
+                    // TODO: the path may  be deeper than the mapping
+                    // in other words the path needs to check if it 
+                    // starts with the one in the mapping 
+                    // then figure out the additional folders
+
                     // Find a mapping for the directory to see if we are changing it
                     // so we can figure out what the new directory will be and fix the reference
-                    var refMapping = directoryMappings.FirstOrDefault(dm => dm.from.TrimEnd('/', '\\') == refDirRelativePath);
+                    var refMapping = directoryMappings.FirstOrDefault(dm =>
+                        IsSamePathOrDeeper(refDirRelativePath, dm.from));
 
+                    // See if the reference is further into the folder path than just the mapping
+                    // since it's possible the reference is inside of one or more subfolders 
+                    // beyond what the mapping specified
+                    var refPathSubDirs = new DirectoryPath("./");
+                    
+                    if (rp.Contains("Xamarin.Forms.Platform.MacOS.csproj")
+                        )
+                        context.Log.Information("..\\Xamarin.Forms.Core\\Xamarin.Forms.Core.csproj");
+
+
+                    if (refMapping != default)
+                    {
+                        var fromBase = new DirectoryPath(Consts.BasePath).Combine(refMapping.from);
+                        refPathSubDirs = fromBase.GetRelativePath(refFullPath.GetDirectory());
+                    }
+
+                    // We only care about SUB-dirs, deeper than the original reference's path
+                    // check if the relative difference would have us backing out to a higher path
+                    // and ignore those
+                    //if (refPathSubDirs.FullPath.StartsWith(".."))
+                    //    refPathSubDirs = new DirectoryPath("./");
+                        
                     var currentFilename = refFullPath.GetFilename();
 
                     var refFileMapping = fileMappings.FirstOrDefault(fm => fm.from.Equals(currentFilename.FullPath));
@@ -121,11 +184,11 @@ public sealed class DefaultTask : FrostingTask<BuildContext>
 
 
                     // The reference might have its file renamed
-                    var newRefFull = refFullPath.GetDirectory().CombineWithFilePath(renamedFilename);
+                    var newRefFull = refFullPath.GetDirectory().Combine(refPathSubDirs).CombineWithFilePath(renamedFilename);
 
                     // The reference also be getting moved to another folder
                     if (refMapping != default)
-                        newRefFull = (new DirectoryPath(Consts.BasePath)).Combine(refMapping.to).CombineWithFilePath(renamedFilename);
+                        newRefFull = (new DirectoryPath(Consts.BasePath)).Combine(refMapping.to).Combine(refPathSubDirs).CombineWithFilePath(renamedFilename);
 
                     // Get the new file location path but relative to the base working dir
                     var newFileRelativePath = file.GetDirectory().FullPath.Replace(Consts.BasePath, "./").TrimEnd('/', '\\');
@@ -143,7 +206,7 @@ public sealed class DefaultTask : FrostingTask<BuildContext>
 
                     var finalnewref = newRefRelativeToFile.GetRelativePath(newRefFull);
 
-                    text = text.Replace(rp, PlatformFriendlyPath(context, finalnewref));
+                    text = text.Replace(rp, MSBuildifyPath(finalnewref));
                 }
             }
 
@@ -194,9 +257,32 @@ public sealed class DefaultTask : FrostingTask<BuildContext>
         }
     }
 
-    static string MacPath(Path path)
+    // ie:  path: './src/Folder/subfolder/file.txt', sameOrDeeperThan: './src/Folder'
+    static bool IsSamePathOrDeeper(DirectoryPath path, DirectoryPath sameOrDeeperThan)
     {
-        return path.FullPath.Replace('\\', '/');
+        // If it's not at least as deep as the same or deeper, obvious false
+        if (path.Segments.Length < sameOrDeeperThan.Segments.Length)
+            return false;
+
+        for (int i = 0; i < path.Segments.Length; i++)
+        {
+            // If we make it to where all segments matched so far
+            // and we've compared all the segments in the sameOrDeeperThan
+            // then we are deeper
+            if (i >= sameOrDeeperThan.Segments.Length)
+                return true;
+
+            // If the current segments don't match, it's not the same path
+            if (path.Segments[i] != sameOrDeeperThan.Segments[i])
+                return false;
+        }
+
+        return true;
+    }
+
+    static string MSBuildifyPath(Path path)
+    {
+        return path.FullPath.Replace("/./", "/").Replace('/', '\\');
     }
 
     static string PlatformFriendlyPath(ICakeContext context, Path path)
